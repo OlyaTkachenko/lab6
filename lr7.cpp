fe1f2540ed9913461b6e41e70900f78082c72829
Olya Tkachenko <olatkacenko940@gmail.com>
2024-10-18 20:35:17 +0300

    modified code

--- lr7.cpp
+++ lr7.cpp
@@ -7,12 +7,12 @@ int main()
    	 double y, x, x1, x2, h;
    	 cout << "Enter [x1; x2] and a step h:\n";
    	 cin >> x1 >> x2 >> h;
-   	 cout << "*************WHILE************\n";
+   	 cout << "*************DO_WHILE************\n";
    	 x = x1;
-    	    while (x<=3+h/2){
+    	    do {
      	       y = ((pow(x, 8)) + 5 * (pow(x, 2)) - 5);
       	      cout << setw(20) <<left << x << setw(10) << right<<y << endl;
       	      x = x + 0.2;
-       	 }
+            } while (x <= 3 + h / 2);
 
 }
