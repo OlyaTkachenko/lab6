64d69feab4e38c2e43b054aade030d0caa71b2a3
Olya Tkachenko <olatkacenko940@gmail.com>
2024-10-30 10:51:51 +0200

    code do_while/while

--- lr7.cpp
+++ lr7.cpp
@@ -14,5 +14,10 @@ int main()
       	      cout << setw(20) <<left << x << setw(10) << right<<y << endl;
       	      x = x + 0.2;
             } while (x <= 3 + h / 2);
-
-}
+        cout << "*************WHILE************\n";
+            x = x1;
+            while (x <= 3 + h / 2) {
+                y = ((pow(x, 8)) + 5 * (pow(x, 2)) - 5);
+                cout << setw(20) << left << x << setw(10) << right << y << endl;
+                x = x + 0.2;
+            } 
