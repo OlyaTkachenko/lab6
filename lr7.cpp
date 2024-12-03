a20c042e4d0d2368ee6826dd7c8ec498545adc0c
Olya Tkachenko <olatkacenko940@gmail.com>
2024-12-03 22:17:10 +0200

    add func

--- lr7.cpp
+++ lr7.cpp
@@ -2,6 +2,7 @@
 #include <iomanip>
 #include <math.h>
 using namespace std;
+double circle(doble x, double h);
 int main()
 {
    	 double y, x, x1, x2, h;
@@ -9,15 +10,18 @@ int main()
    	 cin >> x1 >> x2 >> h;
    	 cout << "*************DO_WHILE************\n";
    	 x = x1;
-    	    do {
-     	       y = ((pow(x, 8)) + 5 * (pow(x, 2)) - 5);
-      	      cout << setw(20) <<left << x << setw(10) << right<<y << endl;
-      	      x = x + 0.2;
-            } while (x <= 3 + h / 2);
-        cout << "*************WHILE************\n";
-            x = x1;
-            while (x <= 3 + h / 2) {
-                y = ((pow(x, 8)) + 5 * (pow(x, 2)) - 5);
-                cout << setw(20) << left << x << setw(10) << right << y << endl;
-                x = x + 0.2;
-            } 
+     double m = circle(x, h);
+     cout << m << endl;
+         }
+     }
+     double circle( doble x, double h) {
+         do {
+             double y = ((pow(x, 8)) + 5 * (pow(x, 2)) - 5);
+             cout << setw(20) << left << x << setw(10) << right << y << endl;
+             double x = x + 0.2;
+         } while (x <= 3 + h / 2);
+         while (x <= 3 + h / 2) {
+             y = ((pow(x, 8)) + 5 * (pow(x, 2)) - 5);
+             cout << setw(20) << left << x << setw(10) << right << y << endl;
+             double x = x + 0.2;
+         }
