f02322a5c51b119c1cf91b15221707a34af9abe2
Olya Tkachenko <olatkacenko940@gmail.com>
2024-12-03 22:44:18 +0200

    array

--- new_branch.cpp
+++ new_branch.cpp
@@ -2,8 +2,17 @@
 using namespace std;
 int main()
 {
-    int n = 100, p = 3, m = 25, s = 4;
+    int n = 100, p = 3, m = 25, const s = 4;
     double h;
     h = (n - (m * p)) / s;
     printf("H = %lf ", h);
+    int a[s][s];
+    srand(time(NULL));
+    for (i = 0; i < s; i++) {
+        for (j = 0; j < s; j++) {
+            a[i][j] = rand() % 21 - 10;
+            cout << '\t' << a[i][j];
+        }
+        cout << endl;
+ }
 }
